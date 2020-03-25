306c0456eef2f310bf3e673760d2e8310e72e0a4
dtring <49992154+dtring@users.noreply.github.com>
2020-03-25 21:04:06 +0000

    Overhaul begins!

---
+++ JavaScript/main.js
@@ -0,0 +1,19 @@
+
+function showMenu() {
+    var x = document.getElementById("sidebar-content");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+    document.addEventListener("DOMContentLoaded", function(showMenu) { 
+      document.getElementById("hamburger").click();
+      });
+
+
+
+
+
+      
